f2c0acb05cc6f2a84501869a543fb0a5ab8161f5
Maciej Monkiewicz <70110809+juanmeister420@users.noreply.github.com>
2022-10-20 09:18:48 +0200

    Update cw_20.10.py

--- programowanie_obiektowe/klasa 4/cw_20.10.py
+++ programowanie_obiektowe/klasa 4/cw_20.10.py
@@ -1,31 +1,25 @@
-# create KontoBankowe class with saldo attribute
-# add method wplata(), which adds money to saldo
-# add method wyplata(), which subtracts money from saldo (if there is enough money)
+# create class called "Sklep" with attributes: name, address, products
+# add a destructor to the class
+# create a method called "add_product" which adds a product to the list of products
+# create a method called "remove_product" which removes a product from the list of products
+# create a method called "show_products" which prints all products
 
-class KontoBankowe:
-    def __init__(self, saldo):
-        self.saldo = saldo
+class Sklep:
+    def __init__(self, name, address):
+        self.name = name
+        self.address = address
+        self.products = []
+    def __del__(self):
+        print("Usuwam sklep")
+    def add_product(self, product):
+        self.products.append(product)
+    def remove_product(self, product):
+        self.products.remove(product)
+    def show_products(self):
+        print(self.products)
 
-    def wplata(self, kwota):
-        self.saldo += kwota
+sklep = Sklep("Sklep", "ul. Sklepowa 1")
+sklep.add_product("Produkt 1")
+sklep.add_product("Produkt 2")
 
-    def wyplata(self, kwota):
-        if self.saldo >= kwota:
-            self.saldo -= kwota
-            return True
-        else:
-            print("Brak wystarczających środków na koncie")
-            return False
-
-
-
-konto = KontoBankowe(1000)
-konto_2 = KontoBankowe(2000)
-
-print(f"Saldo konta 1: {konto.saldo} \nSaldo konta 2: {konto_2.saldo}")
-print("Wpłata 3000 zł na konto 1")
-konto.wplata(3000)
-print(f"Saldo konta 1: {konto.saldo} \nSaldo konta 2: {konto_2.saldo}")
-print("Wypłata 100 zł z konta 2")
-konto_2.wyplata(100)
-print(f"Saldo konta 1: {konto.saldo} \nSaldo konta 2: {konto_2.saldo}")
+sklep.show_products()
